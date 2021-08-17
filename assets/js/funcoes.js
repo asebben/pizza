9807c656b208fc8ad43ab807c9ca2c2ec2ab48d3
asebben <andressa_sebben@yahoo.com>
2021-08-16 21:32:36 -0300

    Aula 16/08

--- assets/js/funcoes.js
+++ assets/js/funcoes.js
@@ -5,3 +5,21 @@ function showMenu(){
     else
         menu.style.display = "block";   
 }
+
+function closeHelp(){
+    document.getElementById("duvidas").style.display = "none";
+}
+
+function minHelp(){
+    let div = document.getElementById("duvidas");
+    div.style.height = "35px";
+    let text = document.getElementById("texto");
+    text.innerHTML = "Dúvidas?";
+}
+
+function maxHelp(){
+    let div = document.getElementById("duvidas");
+    div.style.height = "150px";
+    let text = document.getElementById("texto");
+    text.innerHTML = "Dúvidas? Fale com nossos atendentes";
+}
